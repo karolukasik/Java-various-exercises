68d7ddf0a95bb084286e2c36b200642ba1fce70d
karolukasik <karo.lukasik@gmail.com>
2022-08-06 13:20:11 +0200

    fixed InsertionSort

--- variousexercises/sorting-algorithms/demo/src/main/java/com/karolina/sortingalgorithms/InsertionSort.java
+++ variousexercises/sorting-algorithms/demo/src/main/java/com/karolina/sortingalgorithms/InsertionSort.java
@@ -11,14 +11,19 @@ public class InsertionSort<T extends Comparable<T>> extends SortingAlgorithm<T> 
             int startingPos;
             T comparedValue;
             T originalValue;
+
             for (int i = 0; i < list.size() - 1; i++) {
                 startingPos = i + 1;
+
                 for (int j = startingPos - 1; j >= 0; j--) {
                     comparedValue = list.get(j);
-                    originalValue = list.get(startingPos);
-                    if (comparedValue.compareTo(originalValue) < 0) {
-                        list.set(startingPos, comparedValue);
+                    originalValue = list.get(j + 1);
+
+                    if (originalValue.compareTo(comparedValue) < 0) {
+                        list.set(j + 1, comparedValue);
                         list.set(j, originalValue);
+                    } else {
+                        break;
                     }
                 }
             }
